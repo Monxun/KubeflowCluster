20c9c441981fcd0d96bdf2bd170319f11cd36712
Michael Gibney <michaelgibney1@gmail.com>
2022-05-28 22:35:48 -0700

    updated ansible

--- ansible/terraform/aws/static/destroy.sh
+++ ansible/terraform/aws/static/destroy.sh
@@ -2,6 +2,15 @@
 
 terraform init
 
+# DELETE EIN'S FIRST 
+terraform destroy \
+    -target=aws_eip.nat_a \
+    -lock=false 
+
+terraform destroy \
+    -target=aws_eip.nat_b \
+    -lock=false 
+
 terraform destroy \
     -auto-approve=true \
-    -lock=false
+    -lock=false 
