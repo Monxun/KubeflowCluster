dccb31d044f31aacc87a15f77951857170013390
Michael Gibney <michaelgibney1@gmail.com>
2022-06-01 01:18:18 -0700

    Updated Ansible Files

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,3 @@
 cd ansible
 ansible-playbook DEPLOY_kubeflow_cluster_playbook.yaml --extra-vars \
     "destroy=false cloud_provider='aws'"
-
-k9s
--- destroy.sh
+++ destroy.sh
@@ -3,4 +3,3 @@
 cd ansible
 ansible-playbook DESTROY_kubeflow_cluster_playbook.yaml --extra-vars \
     "destroy=true cloud_provider='aws'"
-k9s
