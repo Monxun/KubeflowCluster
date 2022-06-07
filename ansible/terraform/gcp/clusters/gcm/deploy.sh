8cfb70f561896f560a5e2975ee80e7086644ed8a
Michael Gibney <michaelgibney1@gmail.com>
2022-06-07 14:12:33 -0700

    aded deploy script

---
+++ ansible/terraform/gcp/clusters/gcm/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+export DEPLOYMENT_NAME=${PWD##*/}  
+
+# Start from a clean slate
+rm -rf .terraform
+
+terraform init -lock=false
+
+terraform plan \
+    -lock=false \
+    -input=false \
+    -out=tf-$DEPLOYMENT_NAME.plan
+
+terraform apply \
+    -input=false \
+    -auto-approve=true \
+    -lock=false \
+    tf-$DEPLOYMENT_NAME.plan
+
+# GRAB KUBECONFIG FOR NEW CLUSTER
+gcloud container clusters get-credentials kubeflow-cluster-gke --zone=$GCP_REGION
